6c2c3d570bde2d62b5ee0424eb4c714a63d000ff
flaviojnr100 <45611121+flaviojnr100@users.noreply.github.com>
2020-10-30 21:07:48 -0300

    Delete admin.py

--- back_end/projeto/pizzaria/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Usuario,Pedido,Cardapio
-
-admin.site.register(Usuario)
-admin.site.register(Pedido)
-admin.site.register(Cardapio)
